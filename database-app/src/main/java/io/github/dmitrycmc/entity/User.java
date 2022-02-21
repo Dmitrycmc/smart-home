477ff8da3e3962c72dbc0303fbe588eaf06f2a64
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-02-21 07:14:33 +0300

    Liquibase

---
+++ database-app/src/main/java/io/github/dmitrycmc/entity/User.java
@@ -0,0 +1,24 @@
+package io.github.dmitrycmc.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    @Column(nullable = false)
+    private String password1;
+}
